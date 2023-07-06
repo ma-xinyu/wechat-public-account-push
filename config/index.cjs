0f15af61f4ece7c5e97d40b5b46030bdd65c6f69
马薪宇 <74640322+ma-xinyu@users.noreply.github.com>
2023-07-06 17:02:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oNz336KH-1qRa7tx8xAq30exAAfs	',
+      id: 'oNz336KH-1qRa7tx8xAq30exAAfs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'e57aULwefXwGv_tZ0K1uufCXay5ovdklutS1dor-biA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
